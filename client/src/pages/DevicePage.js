252e8484bc681a7f49a2ad02a809347ac640e74f
Frozen-Dark <atljhfirj2015@gmail.com>
2022-12-14 18:50:43 +0300

    Commit

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import {Col, Container} from "react-bootstrap";
-
+import {Col, Container, Image, Row} from "react-bootstrap";
+import BigStar from '../assets/Star 1.svg'
 const DevicePage = () => {
-    return (
-        <Container>
-            <Col md={4}>
 
+    const device  = {id: 1, name: "Iphone 12 pro", price: 25000, rating: 5, img: `https://www.purposechurch.com/wp-content/uploads/2017/10/fpo400x300.png`}
+    return (
+        <Container className="mt-3">
+            <Col md={4}>
+                <Image width={300} height={300} src={device.img}/>
             </Col>
 
             <Col md={4}>
-
+                <Row className="d-flex flex-column align-items-center">
+                    <h2>{device.name}</h2>
+                    <div
+                        className="d-flex align-items-center justify-content-between"
+                        style={{width: 240, height: 240, backgroundSize: "cover"}} //!@!!! 1 47
+                    >
+                        {device.rating}
+                    </div>
+                </Row>
             </Col>
 
             <Col md={4}>
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -13,7 +13,7 @@ const generateJwt = (id, email, role) => {
 }
 
 class UserController {
-    async registration(req, res) {
+    async registration(req, res, next) {
         const{email, password, role} = req.body
         if(!email || !password) {
             return next(ApiError.badRequest('Неккоректная почта или пароль'))
